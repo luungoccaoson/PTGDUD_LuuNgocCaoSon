7d04fcf1aa2aea0799ae0afe47e2c142fb8d17b4
H82M19\Student <Student>
2025-01-08 10:29:42 +0700

    Part1_lesson2

---
+++ bai2.js
@@ -0,0 +1,29 @@
+var massMark1 = 78;
+var heightMark1 = 1.69;
+var massJohn1 = 92;
+var heightJohn1 = 1.95;
+
+var markBMI1 = (massMark1/(heightMark1*heightMark1));
+var johnBMI1 = (massJohn1/(heightJohn1*heightJohn1));
+
+
+if (markBMI1 > johnBMI1) {
+    console.log(`Mark's BMI (${markBMI1.toFixed(2)}) is higher than John's (${johnBMI1.toFixed(2)})!`);
+  } else {
+    console.log(`John's BMI (${johnBMI1.toFixed(2)}) is higher than Mark's (${markBMI1.toFixed(2)})!`);
+  }
+
+var massMark2 = 95;
+var heightMark2 = 1.88;
+var massJohn2 = 85;
+var heightJohn2 = 1.76;
+
+var markBMI2 = (massMark2/(heightMark2*heightMark2));
+var johnBMI2 = (massJohn2/(heightJohn2*heightJohn2));
+
+if (markBMI2 > johnBMI2) {
+    console.log(`Mark's BMI (${markBMI2.toFixed(2)}) is higher than John's (${johnBMI2.toFixed(2)})!`);
+  } else {
+    console.log(`John's BMI (${johnBMI2.toFixed(2)}) is higher than Mark's (${markBMI2.toFixed(2)})!`);
+  }
+
