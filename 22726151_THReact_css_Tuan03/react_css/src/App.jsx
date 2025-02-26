c22d337010399deb56b29b45efbf89473f3dc840
Lưu Ngọc Cao Sơn <156668941+luungoccaoson@users.noreply.github.com>
2025-02-26 17:20:00 +0700

    Tuan3_baitap02-minhchung

--- 22726151_THReact_css_Tuan03/react_css/src/App.jsx
+++ 22726151_THReact_css_Tuan03/react_css/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Bai1 from './component/bai01'
+import Bai2 from './component/bai02'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Bai1></Bai1>
+      {/* <Bai1></Bai1> */}
+      <Bai2></Bai2>
     </>
   )
 }
