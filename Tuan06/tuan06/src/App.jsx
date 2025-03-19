1b9e48b61be160c92926c844ee48f46813839ec1
H82M19\Student <Student>
2025-03-19 11:14:04 +0700

    bai2

--- Tuan06/tuan06/src/App.jsx
+++ Tuan06/tuan06/src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Bai1 from './components/bai1'
+import { useState } from 'react';
+import './App.css';
+import Bai1 from './components/bai1';
+import Bai2 from './components/bai2';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedComponent, setSelectedComponent] = useState('Bai1');
+
+  const renderComponent = () => {
+    switch (selectedComponent) {
+      case 'Bai1':
+        return <Bai1 />;
+      case 'Bai2':
+        return <Bai2 />;
+      default:
+        return <Bai1 />;
+    }
+  };
 
   return (
-    <>
-      <Bai1/>
-    </>
-  )
+    <div className="App">
+      <nav>
+        <button onClick={() => setSelectedComponent('Bai1')}>Bài 1</button>
+        <button onClick={() => setSelectedComponent('Bai2')}>Bài 2</button>
+      </nav>
+      <div className="content">
+        {renderComponent()}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Tuan06/tuan06/src/components/bai2.jsx
+++ Tuan06/tuan06/src/components/bai2.jsx
@@ -1,10 +1,63 @@
-import React, { useReducer, useRef, useState } from 'react'
-import './bai1.css'
+import React, { useEffect, useReducer, useRef, useState } from 'react'
+import './bai2.css'
 
 export default function Bai2() {
-    return(
+
+    var [data, setData] = useState([]);
+    var [editIndex, setEditIndex] = useState(null);
+    var inputRef = useRef();
+
+    useEffect(() => {
+        var url = ''; 
+        fetch(url)
+            .then((res) => res.json())
+            .then((dataApi) => {
+                setData(dataApi);
+            })
+    }, []);
+
+    function addTodo() {
+        var newData = inputRef.current.value;
+        if (newData === '') return;
+        if (editIndex !== null) {
+            var updateTodos = [...data];
+            updateTodos[editIndex] = newData;
+            setData(updateTodos);
+            setEditIndex(null);
+        } else {
+            setData([...data, newData]);
+        }
+        inputRef.current.value = '';
+    }
+
+    function deleteTodo(index) {
+        var updatedTodos = data.filter((_, i) => i !== index);
+        setData(updatedTodos);
+    };
+
+    function editTodo(index) {
+        setEditIndex(index);
+        inputRef.current.value = data[index];
+    };
+
+    return (
         <>
-            
+            <div className="todo-container">
+                <h2>Todo List</h2>
+                <div className="todo-input-container">
+                    <input ref={inputRef} type="text" />
+                    <button onClick={addTodo}>{editIndex !== null ? 'Save' : 'Add'}</button>
+                </div>
+                <ul className="todo-list">
+                    {data.map((todo, index) => (
+                        <li key={index}>
+                            <span>{todo}</span>
+                            <button onClick={() => editTodo(index)}>Edit</button>
+                            <button onClick={() => deleteTodo(index)}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
         </>
     );
 }
