ddd9f20a5995d4418b692862cde5eb47087d7363
H82M19\Student <Student>
2025-04-09 10:25:49 +0700

    5-7 Navlink-reactRoute + minhchung

--- Tuan07/tkreact/src/App.jsx
+++ Tuan07/tkreact/src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Root from './components/Root.jsx'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
@@ -8,19 +7,19 @@ function App() {
 
   return (
     <>
-      <Router>
+      <Router> 
       <div className="grid grid-cols-[0.7fr_2fr] gap-1 p-1 h-screen">
         <Sidebar />
 
-        <div className="grid grid-rows-[0.7fr_2fr_5fr] gap-1 border-2 border-gray-100 shadow-md">
+        <div className="grid grid-rows-[0.7fr_2fr_5fr] gap-1 border-2 border-gray-100 shadow-md"> 
           <Routes>
             <Route path="/" element={<Dashboard />} />
           </Routes>
-        </div>
+        </div> 
       </div>
       </Router>
     </>
   )
 }
 
-export default App
+export default App 
