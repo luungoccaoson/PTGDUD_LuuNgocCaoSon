4c4490626f576575935697f0e2d34077605ba72e
H82M19\Student <Student>
2025-04-09 10:29:53 +0700

    8. Click edit modal + minhchung

--- Tuan07/tkreact/src/components/EditModal.jsx
+++ Tuan07/tkreact/src/components/EditModal.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 
 const EditModal = ({ user, onClose, onSave }) => {
   const [formData, setFormData] = useState({ ...user });
+  //
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const handleSubmit = () => {
+    //PUT
     fetch(`https://67e368a62ae442db76d0012e.mockapi.io/user/${user.id}`, {
       method: "PUT",
       headers: {
@@ -21,6 +23,7 @@ const EditModal = ({ user, onClose, onSave }) => {
   };
 
   return (
+    //tailwindcss
     <div className="fixed inset-0 backdrop-blur-sm flex items-center justify-center">
       <div className="bg-white border-1 p-6 rounded-lg w-96">
         <h2 className="text-lg font-bold mb-4">Edit User</h2>
@@ -28,7 +31,7 @@ const EditModal = ({ user, onClose, onSave }) => {
           <input
             type="text"
             name="customerName"
-            value={formData.customerName}
+            value={formData.customerName} 
             onChange={handleChange}
             placeholder="Customer Name"
             className="w-full p-2 border rounded-lg"
@@ -38,7 +41,7 @@ const EditModal = ({ user, onClose, onSave }) => {
             name="company"
             value={formData.company}
             onChange={handleChange}
-            placeholder="Company"
+            placeholder="Company" 
             className="w-full p-2 border rounded-lg"
           />
           <input
@@ -46,7 +49,7 @@ const EditModal = ({ user, onClose, onSave }) => {
             name="orderValue"
             value={formData.orderValue}
             onChange={handleChange}
-            placeholder="Order Value"
+            placeholder="Order Value" 
             className="w-full p-2 border rounded-lg"
           />
           <input
@@ -54,7 +57,7 @@ const EditModal = ({ user, onClose, onSave }) => {
             name="orderDate"
             value={formData.orderDate}
             onChange={handleChange}
-            placeholder="Order Date"
+            placeholder="Order Date" 
             className="w-full p-2 border rounded-lg"
           />
           <select
